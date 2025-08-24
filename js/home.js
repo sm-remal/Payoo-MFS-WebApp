9a76fe9079a2ab5e1e19fd8c8fd50505e8a7b700
sm-remal <remal.mozzem@gmail.com>
2025-08-24 14:20:04 +0600

    update

--- js/home.js
+++ js/home.js
@@ -1,6 +1,8 @@
 // Add Money Feature Calculation
 const setPin = 1234;
 const numberRegex = /^[0-9]+$/
+const transactionData = [];
+
 document.getElementById('add-money-btn').addEventListener('click',(event) => {
     event.preventDefault();
     const availableBalance = parseInt(document.getElementById("available-balance").innerText);
@@ -27,8 +29,14 @@ document.getElementById('add-money-btn').addEventListener('click',(event) => {
     const totalAmount = availableBalance + addAmount
     // availableBalance.innerText = totalAmount;
     document.getElementById("available-balance").innerText = totalAmount
-})
 
+    const data = {
+        Name: "Add Money",
+        Date: new Date().toLocaleTimeString()
+    }
+    transactionData.push(data)
+    console.log(transactionData)
+})
 
 // Cash Out Feature Calculation
 document.getElementById("withdraw-btn").addEventListener("click",function(event){
@@ -52,8 +60,14 @@ document.getElementById("withdraw-btn").addEventListener("click",function(event)
     }
     const newBalance = availableBalance - cashOutAmount;
     document.getElementById("available-balance").innerText = newBalance;
-})
 
+    const data = {
+        Name: "Cash Out",
+        Date: new Date().toLocaleTimeString()
+    }
+    transactionData.push(data)
+    console.log(transactionData)
+})
 
 // Transfer Money feature calculation
 
@@ -78,15 +92,20 @@ document.getElementById("transfer-money-btn").addEventListener("click", function
     }
     const remainingAmount = availableBalance - transferAmount;
     document.getElementById("available-balance").innerText = remainingAmount;
-})
 
+    const data = {
+        Name: "Transfer Money",
+        Date: new Date().toLocaleTimeString()
+    }
+    transactionData.push(data)
+    console.log(transactionData)
+})
 
 // Get Bonus feature calculation 
 
 document.getElementById("get-bonus-btn").addEventListener("click", function(event){
     event.preventDefault();
 })
-
 
 // Pay Bill feature calculation 
 
@@ -117,8 +136,43 @@ document.getElementById("pay-bill-btn").addEventListener("click", function(event
     const remainingBalance = availableBalance - payAmount;
     document.getElementById("available-balance").innerText = remainingBalance;
 
+    const data = {
+        Name: "Pay Bill",
+        Date: new Date().toLocaleTimeString()
+
+    }
+    transactionData.push(data)
+    console.log(transactionData)
+
 })
 
+// Transaction feature calculation 
+
+document.getElementById("btn-transaction").addEventListener("click",function(){
+    console.log(transactionData)
+    const transactionContainer = document.getElementById("transaction-container");
+    transactionContainer.innerText = ""
+    for(const main of transactionData){
+        const createElementDiv = document.createElement("div");
+        createElementDiv.innerHTML = `
+
+        <div class="max-w-md mx-auto w-full flex justify-between items-center pr-3 py-2 bg-white my-4 rounded-sm">
+            <div class="flex items-center gap-6 pl-3">
+                <div class=" w-[50px] h-[50Px] flex justify-center items-center bg rounded-full bg-gray-100">
+                    <img src="./assets/wallet1.png" alt="" width="30px" height="30px">
+                </div>
+                <div>
+                    <h2 class="font-bold">${main.Name}</h2>
+                    <p class="text-gray-500">${main.Date  }</p>
+                </div>
+            </div>
+            <i class="fa-solid fa-ellipsis-vertical"></i>
+        </div>
+        </div>
+        ` 
+       transactionContainer.appendChild(createElementDiv)
+    }
+})
 
 // Toggling 
 
@@ -127,15 +181,19 @@ const cashOutSection = document.getElementById("cash-out-section");
 const transferMoneySection = document.getElementById("transfer-money-section");
 const getBonusSection = document.getElementById("get-bonus-section")
 const payBillSection = document.getElementById("pay-bill-section")
+const transaction = document.getElementById("transaction-section");
 
 
 // add btn 
 document.getElementById("btn-add-money").addEventListener("click",function(){
+    addMoneySection.style.borderColor = "blue"
     cashOutSection.style.display = "none"
     transferMoneySection.style.display = "none"
     getBonusSection.style.display = "none"
     payBillSection.style.display = "none"
     addMoneySection.style.display = "block"
+    transaction.style.display= "none"
+    
 })
 
 // cash out 
@@ -145,6 +203,7 @@ document.getElementById("btn-cash-out").addEventListener("click",function(){
     getBonusSection.style.display = "none"
     payBillSection.style.display = "none"
     addMoneySection.style.display = "none"
+    transaction.style.display= "none"
 })
 
 // transfer 
@@ -154,6 +213,7 @@ document.getElementById("btn-transfer").addEventListener("click",function(){
     getBonusSection.style.display = "none"
     payBillSection.style.display = "none"
     addMoneySection.style.display = "none"
+    transaction.style.display= "none"
 })
 
 // get bonus 
@@ -163,6 +223,7 @@ document.getElementById("btn-get-bonus").addEventListener("click",function(){
     getBonusSection.style.display = "block"
     payBillSection.style.display = "none"
     addMoneySection.style.display = "none"
+    transaction.style.display= "none"
 })
 
 // pay bill 
@@ -172,4 +233,17 @@ document.getElementById("btn-pay-bill").addEventListener("click",function(){
     getBonusSection.style.display = "none"
     payBillSection.style.display = "block"
     addMoneySection.style.display = "none"
+    transaction.style.display= "none"
+})
+
+// Transaction 
+
+document.getElementById("btn-transaction").addEventListener("click",function(){
+    cashOutSection.style.display = "none"
+    transferMoneySection.style.display = "none"
+    getBonusSection.style.display = "none"
+    payBillSection.style.display = "none"
+    addMoneySection.style.display = "none"
+    transaction.style.display= "block"
+
 })
